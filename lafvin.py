6de8607089170138ed7d61b9a28ad7dde1c6d774
kirsh-1 <157482278+kirsh-1@users.noreply.github.com>
2024-02-28 10:03:23 +0300

    Add files via upload

--- lafvin.py
+++ lafvin.py
@@ -1,71 +1,96 @@
-import time
 from serial.serialposix import Serial
+
+import time
+import asyncio
 
 
 class ArduinoConnection:
-    """Стандартный порт /dev/ttyUSB0"""
 
-    def __init__(self, port: str = "/dev/ttyUSB0") -> None:
-        self._serial = Serial(port, baudrate=9600, timeout=.1)
-        time.sleep(2.1)
+    def __init__(self, device: str = "/dev/ttyUSB0") -> None:
+        self._serial = Serial(device, baudrate=9600, timeout=.1)
+        time.sleep(2)
 
 
 class Lafvin(ArduinoConnection):
-    def read_distance(self) -> int:
-        """
-        Считать дистанцию с датчика\n
-        Возвращает int дистанции в см
-        """
-        self._serial.write(b'd')
-        time.sleep(.5)
-        result = self._serial.readline().decode().rstrip()
-        if result:
-            to_int = int(float(result))
-            return to_int
-        else:
-            raise Exception('Seriar.readline() is None')
+    def _send_command(self, command) -> str:
+        self._serial.write(command.encode())
+        response = self.__read_response()
+        return response
 
-    def move_servo(self, angle: int = 90) -> None:
-        """
-        Повернуть сервопривод на ? градусов\n
-        Принимает значения от 0 до 180
-        """
-        self._serial.write("c{}".format(angle).encode())
-        return
+    def __read_response(self) -> str:
+        while True:
+            line = self._serial.readline().decode().rstrip()
+            if line:
+                return line
+            time.sleep(0.1)
 
-    def go_forward(self, speed: int = 100) -> None:
-        """
-        Установить скорость моторов на ?\n
-        Принимает значения от 0 до 255
-        """
-        self._serial.write("f{}".format(speed).encode())
-        return
+    def read_distance(self) -> str:
+        """Возвращает строку дистанции до объекта в см"""
+        return self._send_command("d")
 
-    def rotate_left(self, speed: int = 150) -> None:
-        """
-        Установить скорость правого мотора на ? \n
-        Принимает значения от 0 до 255
-        """
-        self._serial.write("r{}".format(speed).encode())
-        return
+    def move_servo(self, angle: int = 90) -> str:
+        """Поворот сервопривода\nЗначения от 0 до 180"""
+        return self._send_command("c{}".format(angle))
 
-    def rotate_right(self, speed: int = 150) -> None:
-        """
-        Установить скорость левого мотора на ? \n
-        Принимает значения от 0 до 255
-        """
-        self._serial.write("l{}".format(speed).encode())
-        return
+    def go_forward(self, speed: int = 100) -> str:
+        """Устанавливает скорость моторов\nЗначения от 0 до 255"""
+        return self._send_command("f{}".format(speed))
 
-    def go_backward(self, speed: int = 100) -> None:
-        """
-        Установить скорость моторов на ? в реверс\n
-        Принимает значения от 0 до 255
-        """
-        self._serial.write("b{}".format(speed).encode())
-        return
+    def rotate_right(self, speed: int = 150) -> str:
+        """Устанавливает скорость левого мотора\nЗначения от 0 до 255"""
+        return self._send_command("r{}".format(speed))
 
-    def stop(self) -> None:
-        """Остановить моторы\n"""
-        self._serial.write(b's')
-        return
+    def rotate_left(self, speed: int = 150) -> str:
+        """Устанавливает скорость правого мотора\nЗначения от 0 до 255"""
+        return self._send_command("l{}".format(speed))
+
+    def go_backward(self, speed: int = 150) -> str:
+        """Устанавливает скорость моторов в реверс\nЗначения от 0 до 255"""
+        return self._send_command("b{}".format(speed))
+
+    def stop(self):
+        """Останавливает моторы"""
+        return self._send_command("s")
+
+
+class AsyncLafvin(ArduinoConnection):
+
+    async def _send_command(self, command) -> str:
+        self._serial.write(command.encode())
+        response = await self.__read_response()
+        return response
+
+    async def __read_response(self) -> str:
+        while True:
+            line = self._serial.readline().decode('utf-8').rstrip()
+            if line:
+                return line
+            await asyncio.sleep(0.1)
+
+    async def move_servo(self, angle: int = 90) -> str:
+        """Поворот сервопривода\nЗначения от 0 до 180"""
+        return await self._send_command("c{}".format(angle))
+
+    async def read_distance(self) -> str:
+        """Возвращает строку дистанции до объекта в см"""
+        return await self._send_command("d")
+
+    async def go_forward(self, speed: int = 100) -> str:
+        """Устанавливает скорость моторов\nЗначения от 0 до 255"""
+        return await self._send_command("f{}".format(speed))
+
+    async def go_backward(self, speed: int = 150) -> str:
+        """Устанавливает скорость моторов в реверсе\nЗначения от 0 до 255"""
+        return await self._send_command("b".format(speed))
+
+    async def rotate_left(self, speed: int = 150) -> str:
+        """Устанавливает скорость правого мотора\nЗначения от 0 до 255"""
+        return await self._send_command("l{}".format(speed))
+
+    async def rotate_right(self, speed: int = 150) -> str:
+        """Устанавливает скорость левого мотора\nЗначения от 0 до 255"""
+        return await self._send_command("r{}".format(speed))
+
+    async def stop(self) -> str:
+        """Останавливает моторы"""
+        return await self._send_command("s")
